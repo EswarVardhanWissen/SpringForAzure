66891c0341ff7502a6524fcce363edd8e9abe936
unknown <Eswar.Karri@wissen.com>
2024-06-03 17:17:06 +0530

    first commit

---
+++ src/main/java/com/example/SpringForAzure/SpringForAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringForAzure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringForAzure/SpringForAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringForAzure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
